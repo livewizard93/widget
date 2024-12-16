c1efed8cf0127029801c95fadc7ff72a4b0fd60f
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-12-15 21:02:26 -0500

    Widget Added

---
+++ CelebrationWidget/CelebrationWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  CelebrationWidgetBundle.swift
+//  CelebrationWidget
+//
+//  Created by dev on 12/15/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct CelebrationWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        CelebrationWidget()
+    }
+}
