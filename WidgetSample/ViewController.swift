0995d368205f49a5ba470b34448e07b0a5337ec1
livewizard93 <147813832+livewizard93@users.noreply.github.com>
2024-12-15 21:00:02 -0500

    First Commit

---
+++ WidgetSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WidgetSample
+//
+//  Created by dev on 12/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
